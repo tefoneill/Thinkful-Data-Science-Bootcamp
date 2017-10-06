a984a86a208ffa971dc0eb28ee7ec82027167566
tefoneill <31480932+tefoneill@users.noreply.github.com>
2017-10-05 17:29:13 -0700

    Update Unit 1, Lesson 2.2, Select, From, Where.sql
    Revised

--- SQLite/Unit 1, Lesson 2.2, Select, From, Where.sql
+++ SQLite/Unit 1, Lesson 2.2, Select, From, Where.sql
@@ -1,10 +1,8 @@
-/*
-Drill: Write SQL queries to extract information from the Bay Area Bikeshare dataset.
+--Drill: Write SQL queries to extract information from the Bay Area Bikeshare dataset.
 
-1. The IDs and durations for all trips of duration greater than 500, ordered by duration.
-2. Every column of the stations table for station id 84.
-3. The min temperatures of all the occurrences of rain in zip 94301.
-*/
+--1. The IDs and durations for all trips of duration greater than 500, ordered by duration.
+--2. Every column of the stations table for station id 84.
+--3. The min temperatures of all the occurrences of rain in zip 94301.
 
 --1. The IDs and durations for all trips of duration greater than 500, ordered by duration. (Note here that the duration column appears to be a string, so the result is ordered accordingly.)
 SELECT
@@ -26,7 +24,7 @@ WHERE
 		station_id = 84
 
 
---3. The min temperatures of all the occurrences of rain in zip 94301. (Note that if we wanted to include fog-rain Events, we could add OR Events like 'fog-rain' to the end of the query.)
+--3. The min temperatures of all the occurrences of rain in zip 94301.
 SELECT
 		MinTemperatureF
 FROM
